eaabdf7a827c95f1e1ebd2019336e97bce815fa0
Nara Gomez Kim <naragomezkim@hotmail.com>
2022-07-13 18:19:56 -0600

    Added project

---
+++ web_avanzada_fe/web_avanzada_fe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_avanzada_fe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
