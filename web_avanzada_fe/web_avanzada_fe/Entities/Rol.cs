2a854f9e5a09dc3ccb1a7719f4cd438bef48781f
emanuel-bg <emmanuelbg30@gmail.com>
2022-07-24 20:52:58 -0600

    Cambios de Rol y login

---
+++ web_avanzada_fe/web_avanzada_fe/Entities/Rol.cs
@@ -0,0 +1,8 @@
+﻿namespace web_avanzada_fe.Entities
+{
+    public class Rol
+    {
+        public int idRol { get; set; }
+        public string DescripcionRol { get; set; } = string.Empty;
+    }
+}
---
+++ web_avanzada_fe/web_avanzada_fe/Models/RolModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+using web_avanzada_fe.Entities;
+
+namespace web_avanzada_fe.Models
+{
+    public class RolModel
+    {
+        public List<Rol>? ConsultarRoles(IConfiguration _config, string token)
+        {
+            string rutaBase = _config.GetSection("AppSettings:UrlServicio").Value;
+
+            using (var client = new HttpClient())
+            {
+                string rutaServicio = rutaBase + "api/Rol/MostrarRoles";
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage respuesta = client.GetAsync(rutaServicio).GetAwaiter().GetResult();
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    return respuesta.Content.ReadFromJsonAsync<List<Rol>>().Result;
+                }
+
+                return new List<Rol>();
+            }
+        }
+    }
+}
