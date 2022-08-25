a15584737be03dd61b39b45ea7c94304cf505a89
Nara Gomez Kim <naragomezkim@hotmail.com>
2022-08-24 22:12:16 -0600

    Agregar Navbar y se cambio mascota

---
+++ web_avanzada_fe/web_avanzada_fe/wwwroot/js/Mascota.js
@@ -0,0 +1,24 @@
+﻿function validarNumeros(event) {
+    var key = window.event ? event.keyCode : event.which;
+    if (key < 48 || key > 57) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+function VerificarEspaciosRaza() {
+    $("#Raza").val($("#Raza").val().trim());
+}
+
+function VerificarEspaciosNombre() {
+    $("#NombreM").val($("#NombreM").val().trim());
+
+}
+function VerificarPeso() {
+    if ($("#Peso").val() <= 0) {
+        document.getElementById('validacionPeso').innerHTML = 'Digite un peso valido';
+        return
+    };
+    document.getElementById('validacionPeso').innerHTML = '';
+}
